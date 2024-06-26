a16f747d114aae79baee64a040382d8ac9aa26f4
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-06-26 20:11:54 +0530

    Update authController.js

--- Controller/authController.js
+++ Controller/authController.js
@@ -26,7 +26,7 @@ const handleLogin = async(req,res) =>
               }
           },
             process.env.ACCESS_TOKEN_SECRET ,
-            {expiresIn : '1h'}
+            {expiresIn : '2m'}
           )
         
           const refreshToken =jwt.sign(
