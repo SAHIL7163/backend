ddc1b3089b56a40e5c43797c3103ed23ac8504ef
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-06-26 13:51:11 +0530

    Update allowedOrigins.js

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins =[
-    'https://www.google.com',
+ /*    'https://www.google.com',
     'http://127.0.0.1.5500',
     'http://localhost:3500',
-    'http://localhost:3000',
+    'http://localhost:3000', */
    'https://blogsite-3gsy.onrender.com/'
       ];
 
