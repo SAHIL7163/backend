26d598eb854a60e84b9e3ede176ddde8dd50895e
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-06-26 20:37:14 +0530

    Update allowedOrigins.js

--- Config/allowedOrigins.js
+++ Config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins =[
     'http://127.0.0.1.5500',
     'http://localhost:3500',
     'http://localhost:3000',
-   'https://blogsite-3gsy.onrender.com'
+   'https://blogsite-3gsy.onrender.com',
+    'https://blogsite-api-mi38.onrender.com'
       ];
 
 module.exports = allowedOrigins ;
