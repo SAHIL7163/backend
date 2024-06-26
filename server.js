5921e4d0b09f85e568f7391cd5512126dd0651a8
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-06-26 13:10:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -59,8 +59,15 @@ app.use(session({
     secret: process.env.GOOGLE_CLIENT_SECRET, // Replace with your own secret key
     resave: false,
     saveUninitialized: false,
-  
-}));
+    store: MongoStore.create({ // Configure connect-mongo for session storage
+        mongoUrl: process.env.DATABASE_URI,
+        collectionName: 'sessions'
+    }),
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24 // 1 day
+    }
+})); *
 
 
 
