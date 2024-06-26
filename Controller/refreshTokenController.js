2f3c584474635c44d40c414d1115c7e090671197
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-06-26 20:44:01 +0530

    Update refreshTokenController.js

--- Controller/refreshTokenController.js
+++ Controller/refreshTokenController.js
@@ -37,7 +37,7 @@ const handleRefreshToken = async(req,res) =>
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '2m' }
+                { expiresIn: '1h' }
             );
 
            res.json({accessToken,roles,foundUser});
