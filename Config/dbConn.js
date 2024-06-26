6071868a64fabe21d10612552c66880992f097ac
sahil <sahildhameja13@gmail.com>
2024-06-26 10:11:09 +0530

    changes

--- Config/dbConn.js
+++ Config/dbConn.js
@@ -5,14 +5,13 @@ const connectDB = async () => {
         await mongoose.connect(process.env.DATABASE_URI);
             //, {
             //useUnifiedTopology: true,
-<<<<<<< HEAD
            // useNewUrlParser: true
       //  });
         
-=======
+
             //useNewUrlParser: true
         //});
->>>>>>> bed9962d1b21e0f7858dcc1f4b7f87014c0fdb6d
+
     } catch (err){
        console.error(err);
     }
