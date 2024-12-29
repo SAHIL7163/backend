23f4d070f0a5f15e1858eccae5ca34235dbe83a1
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2024-12-29 17:16:06 +0530

    Update Posts.js
    added comments schema

--- model/Posts.js
+++ model/Posts.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema ;
 
+
+const commentSchema = new Schema(
+    {
+    
+      username: {
+        type: String, // Username of the user who made the comment
+        required: true,
+      },
+      content: {
+        type: String, // Content of the comment
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now, // Timestamp when the comment was created
+      },
+    },
+    { _id: true } // Enable automatic unique IDs for comments
+  );
+
+
+
 const postsSchema = new Schema({
     title : {
         type : String ,
@@ -29,7 +51,19 @@ const postsSchema = new Schema({
     user : {
         type : String ,
         required : true
-    }
+    },
+    likes: { 
+        type: Number,
+         default: 0 
+    },
+    dislikes: { 
+        type: Number,
+         default: 0 
+    },
+    usersLiked: { type: [String], default: [] },  // Array to track users who liked
+    usersDisliked: { type: [String], default: [] },
+    comments: [commentSchema] ,
+
 })
 
 module.exports = mongoose.model('Posts' , postsSchema) ;
